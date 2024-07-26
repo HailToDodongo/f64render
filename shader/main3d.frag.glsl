0d7bfba5a440c26db919340a8fab9528bf9f4902
Max Bebök <beboek.max@gmail.com>
2024-07-26 23:44:16 +0200

    chg: more CC, add TODOs for rest

--- shader/main3d.frag.glsl
+++ shader/main3d.frag.glsl
@@ -10,28 +10,46 @@ in flat ivec4 cc1Color;
 in flat ivec4 cc1Alpha;
 
 uniform sampler2D tex0;
+uniform sampler2D tex1;
 
 out vec4 FragColor;
 
 vec3 cc_fetchColor(in int val, in vec4 comb)
 {
-       if(val == CC_C_COMB ) return comb.rgb;
-  else if(val == CC_C_TEX0 ) return texture_3point(tex0, uv).rgb;
-  else if(val == CC_C_SHADE) return cc_shade.rgb;
-  else if(val == CC_C_ENV  ) return cc_env.rgb;
-  else if(val == CC_C_PRIM ) return cc_prim.rgb;
-  else if(val == CC_C_1    ) return vec3(1.0);
+       if(val == CC_C_COMB       ) return comb.rgb;
+  else if(val == CC_C_TEX0       ) return texture_3point(tex0, uv).rgb;
+  else if(val == CC_C_TEX1       ) return texture_3point(tex1, uv).rgb;
+  else if(val == CC_C_PRIM       ) return cc_prim.rgb;
+  else if(val == CC_C_SHADE      ) return cc_shade.rgb;
+  else if(val == CC_C_ENV        ) return cc_env.rgb;
+  // else if(val == CC_C_CENTER     ) return vec3(0.0); // @TODO
+  // else if(val == CC_C_SCALE      ) return vec3(0.0); // @TODO
+  else if(val == CC_C_COMB_ALPHA ) return comb.aaa;
+  else if(val == CC_C_TEX0_ALPHA ) return texture_3point(tex0, uv).aaa;
+  else if(val == CC_C_TEX1_ALPHA ) return texture_3point(tex1, uv).aaa;
+  else if(val == CC_C_PRIM_ALPHA ) return cc_prim.aaa;
+  else if(val == CC_C_SHADE_ALPHA) return cc_shade.aaa;
+  else if(val == CC_C_ENV_ALPHA  ) return cc_env.aaa;
+  // else if(val == CC_C_LOD_FRAC   ) return vec3(0.0); // @TODO
+  // else if(val == CC_C_PRIM_LOD_FRAC) return vec3(0.0); // @TODO
+  // else if(val == CC_C_NOISE      ) return vec3(0.0); // @TODO
+  // else if(val == CC_C_K4         ) return vec3(0.0); // @TODO
+  // else if(val == CC_C_K5         ) return vec3(0.0); // @TODO
+  else if(val == CC_C_1          ) return vec3(1.0);
   return vec3(0.0); // default: CC_C_0
 }
 
 float cc_fetchAlpha(in int val, in vec4 comb)
 {
-         if(val == CC_A_COMB ) return comb.a;
-    else if(val == CC_A_TEX0 ) return texture_3point(tex0, uv).a;
-    else if(val == CC_A_SHADE) return cc_shade.a;
-    else if(val == CC_A_ENV  ) return cc_env.a;
-    else if(val == CC_A_PRIM ) return cc_prim.a;
-    else if(val == CC_A_1    ) return 1.0;
+       if(val == CC_A_COMB ) return comb.a;
+  else if(val == CC_A_TEX0 ) return texture_3point(tex0, uv).a;
+  else if(val == CC_A_TEX1 ) return texture_3point(tex1, uv).a;
+  else if(val == CC_A_PRIM ) return cc_prim.a;
+  else if(val == CC_A_SHADE) return cc_shade.a;
+  else if(val == CC_A_ENV  ) return cc_env.a;
+  // else if(val == CC_A_LOD_FRAC) return 0.0; // @TODO
+  // else if(val == CC_A_PRIM_LOD_FRAC) return 0.0; // @TODO
+  else if(val == CC_A_1    ) return 1.0;
   return 0.0; // default: CC_A_0
 }
 
